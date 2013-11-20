158601e676bf8d107ec4a426eb29194beff0cae1
Mathieu Post <mathieupost@gmail.com>
2013-11-20 16:49:51 +0100

    Add Cell constructor

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,11 @@
 public class Cell {
+	private int row;
+	private int column;
+	private String rawData;
 
+	public Cell(int row, int column, String rawData) {
+		this.row = row;
+		this.column = column;
+		this.rawData = rawData;
+	}
 }
