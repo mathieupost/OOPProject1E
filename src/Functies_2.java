17332376a8f581b26b4ab5555a464fa1aa038faa
wdenhertog <wmdenhertog@gmail.com>
2013-11-20 15:54:59 +0100

    Functies2
    other functions

---
+++ src/Functies_2.java
@@ -0,0 +1,17 @@
+
+public class Functies_2 {
+
+	public static void main(String[] args) {
+		System.out.println(Sum(3,4));
+
+	}
+	
+	public double Sumif(){
+		return 0.0;
+	}
+	
+	public static double Sum(double a, double b){
+		return a + b;
+	}
+
+}
