8ca9bfe9f4412497874d169acafbba6d28b117d4
Casper van der Hout <C.T.vanderhout@student.tudelft.nl>
2013-11-27 16:41:10 +0100

    Lege testcase voor functies

---
+++ src/FunctiesTest.java
@@ -0,0 +1,118 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FunctiesTest {
+
+	@Test
+	public void testAverage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCountA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCountIf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestIf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsLogical() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEven() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMedian() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSqrt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSign() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRoundUp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRoundDown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMin() {
+		fail("Not yet implemented");
+	}
+
+}
