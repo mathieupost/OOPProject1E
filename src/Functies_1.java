f0010b5b369bc658d3167c5090747a616a5a330d
Casper van der Hout <C.T.vanderhout@student.tudelft.nl>
2013-11-27 14:01:50 +0100

    Functies 1 en 2 samenvoegen

--- src/Functies_1.java
+++ src/Functies_1.java
@@ -1,47 +1,50 @@
-import java.util.*;
-//TODO: Commentaar
+//import java.util.*;
+//TODO: Comments
 
 public class Functies_1 {
 
 	public Functies_1() {
-	
+
 	}
 	
-	public static double Average(String[] values) throws NumberFormatException {
-		double res = Double.parseDouble(values[0]);
-		for (int i = 1; i < values.length; i++) {
-			res = (res + Double.parseDouble(values[i]));
+	// DONE
+	// Cel parser takes care of additional functionality (arrays, references to numbers)
+	public static double average(double a, double... values){
+		double res = a;
+		for (int i = 0; i < values.length; i++) {
+			res = (res + values[i]);
 		}
-		res /= values.length;
+		res /= (values.length+1);
 		return res;
 	}
 
-	// TODO: Count
-	public static int Count() {
+	// TODO: Implement when working with cell coördinates
+	// Count cells in range that contain numbers
+	public static int count() {
 		int res = 0;
 		return res;
 	}
 
-	// TODO: CountA
-	public static int CountA() {
+	// TODO: Implement when working with cell coördinates
+	// Count cells in range that are not empty
+	public static int countA() {
 		int res = 0;
 		return res;
 	}
 
-	// TODO: CountIf
-	public static int CountIf() {
+	// TODO: Implement when working with cell coördinates
+	// Count cells in range that meet a given condition
+	public static int countIf() {
 		int res = 0;
 		return res;
 	}
-
-	// Vergelijkt 2 doubles
-	public static String If(String logictest, String iftrue, String iffalse) {
+	
+	// DONE
+	// Compares 2 doubles
+	public static String testIf(String logictest, String iftrue, String iffalse) {
 		String[] split = logictest.split(" ");
 		double a = Double.parseDouble(split[0]);
 		double b = Double.parseDouble(split[2]);
-		// System.out.println(a);
-		// System.out.println(split[1]);
-		// System.out.println(b);
 		if (split[1].equals("=")) {
 			if (a == b) {
 				return iftrue;
@@ -75,33 +78,63 @@ public class Functies_1 {
 		}
 		return iffalse;
 	}
-
-	public int Int(String value) throws NumberFormatException {
-		// rounds down
-		return (int) Math.floor(Double.parseDouble(value));
+	
+	// DONE
+	// Rounds down
+	public int makeInt(double value){		
+		return (int) Math.floor(value);
 	}
-
-	public static boolean Islogical(String value) {
+	
+	// DONE
+	// Checks if String is a logical value
+	public static boolean isLogical(String value) {
 		return value.toLowerCase().equals("true")
 				|| value.toLowerCase().equals("false");
 	}
 
-	public static boolean Iseven(String value) throws NumberFormatException {
+	public static boolean isEven(String value) throws NumberFormatException {
 		return Double.parseDouble(value) % 2 == 0;
 	}
 
-	public static boolean Isnumber(String value) {
-		try { 
-	        Double.parseDouble(value); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
-	    }
-	    return true;
+	public static boolean isNumber(String value) {
+		try {
+			Double.parseDouble(value);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+
+	public static String lower(String value) {
+		return value.toLowerCase();
+	}
+
+	// TODO: ignores locigal values and text
+	public static Double max(double[] values) {
+		double res = values[0];
+		for (int i = 1; i < values.length; i++) {
+			if (values[i] > res) {
+				res = values[i];
+			}
+		}
+		return res;
 	}
 	
-	//TODO: LOWER
+	public static double median(double[] values){
+		double res = 0;
+		if(values.length %2==0){
+			double a = Math.ceil(values.length);
+		}
+		else if(values.length %2 != 0){
+			
+		}
+		return res;
+	}
 
 	public static void main(String[] args) {
+		//System.out.println(average(6,8));
+		//System.out.println(average(6,9));
+		//System.out.println(average(1,3,5));
 		/*
 		 * System.out.println(Iseven(1)); System.out.println(Iseven(2));
 		 * System.out.println(Iseven(1337));
@@ -111,6 +144,7 @@ public class Functies_1 {
 		 * System.out.println(Islogical("\"FALSE\""));
 		 */
 		// double[] array1 = {2.5, 8.43, 1337.0, 1009.29};
+		// System.out.println(max(array1));
 		// System.out.println(Average(array1));
 		/*
 		 * System.out.println(If("13.6 < 13.7","Het is waar!","Het is niet waar!"
