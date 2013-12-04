118596e2544b06a06131a245e3383dc8fd08d3d1
wdenhertog <wmdenhertog@gmail.com>
2013-12-04 17:09:58 +0100

    added last functions
    COUNTA, INT

--- src/main/nl/tudelft/excellence/functions/COUNTA.java
+++ src/main/nl/tudelft/excellence/functions/COUNTA.java
@@ -1,7 +1,5 @@
 package nl.tudelft.excellence.functions;
 
-import java.util.ArrayList;
-
 /**
  * This function counts how many cells in a given range are not empty.
  * <b>Syntax:</b> COUNTA(String a[, String b, String c...])
@@ -9,16 +7,25 @@ import java.util.ArrayList;
 
 public class COUNTA extends NumberFunction{
 	
-	private ArrayList<String> cells;
+	private String[] cells;
 	
 	public COUNTA(String a, String...strings ){
-		
+		cells = new String[strings.length + 1];
+		cells[0] = a;
+		for(int i = 0; i < strings.length; i++){
+			cells[i+1] = strings[i];
+		}
 	}
 
 	@Override
 	public double execute() {
-		// TODO Auto-generated method stub
-		return 0;
+		double result = 0;
+		for(int i = 0; i < cells.length; i++){
+			if(cells[i].equals(null)){
+				result++;
+			}
+		}
+		return result;
 	}
 
 }
--- src/main/nl/tudelft/excellence/functions/INT.java
+++ src/main/nl/tudelft/excellence/functions/INT.java
@@ -4,14 +4,17 @@ package nl.tudelft.excellence.functions;
  * Rounds a double down and converts that to an integer
  * <b>Syntax:</b> INT(double value)
  */
+
 public class INT extends NumberFunction {
+	
+	private double result;
+	
 	public INT(double value){
-		
+		result = value;
 	}
 
 	@Override
 	public double execute() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int)result;
 	}
 }
