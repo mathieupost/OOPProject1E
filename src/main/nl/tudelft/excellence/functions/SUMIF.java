e651e65d3c7e6f38b0e77340f24e4c362895b1a3
Casper van der Hout <C.T.vanderhout@student.tudelft.nl>
2013-12-04 16:38:48 +0100

    Finished SUMIF
    Not yet added documentation

--- src/main/nl/tudelft/excellence/functions/SUMIF.java
+++ src/main/nl/tudelft/excellence/functions/SUMIF.java
@@ -1,20 +1,75 @@
 package nl.tudelft.excellence.functions;
 
 public class SUMIF extends NumberFunction {
-	private String[] range; // values to apply criteria on
-	private double[] sum_range; // values to sum
-	private String criteria; // criteria
+	private double[] range; // values to apply criteria on
+	// private double[] sum_range; // values to sum
+	private double criteria; // criteria 1
+	private String operand; // =, !=, >, <, =>, <=
+	private double sumrangelow;
+	private double sumrangehigh;
+	private double res;
 
-	public SUMIF(String criteria, String[] input_range, double a,
-			double... input_sum_range) {
-		sum_range = new double[input_sum_range.length + 1];
-		sum_range[0] = a;
-		for (int i = 0; i < input_sum_range.length; i++) {
-			sum_range[i + 1] = input_sum_range[i];
-		}
+	public SUMIF(double criteria1, String criteria_operand,
+			double[] input_range, double sum_range_low, double sum_range_high) {
+		range = input_range;
+		criteria = criteria1;
+		operand = criteria_operand;
+		sumrangelow = sum_range_low;
+		sumrangehigh = sum_range_high;
+	}
+
+	public SUMIF(double criteria1, String criteria_operand, double[] input_range) {
+		range = input_range;
+		criteria = criteria1;
+		operand = criteria_operand;
+		sumrangelow = Double.NEGATIVE_INFINITY;
+		sumrangehigh = Double.POSITIVE_INFINITY;
 	}
 
 	public double execute() {
-		return 0;
+		if (operand.equals("=")) {
+			for (int i = 0; i < range.length; i++) {
+				if (range[i] == criteria && range[i] >= sumrangelow
+						&& range[i] <= sumrangehigh) {
+					res += range[i];
+				}
+			}
+		} else if (operand.equals("!=")) {
+			for (int i = 0; i < range.length; i++) {
+				if (range[i] != criteria && range[i] >= sumrangelow
+						&& range[i] <= sumrangehigh) {
+					res += range[i];
+				}
+			}
+		} else if (operand.equals("<")) {
+			for (int i = 0; i < range.length; i++) {
+				if (range[i] < criteria && range[i] >= sumrangelow
+						&& range[i] <= sumrangehigh) {
+					res += range[i];
+				}
+			}
+		} else if (operand.equals(">")) {
+			for (int i = 0; i < range.length; i++) {
+				if (range[i] > criteria && range[i] >= sumrangelow
+						&& range[i] <= sumrangehigh) {
+					res += range[i];
+				}
+			}
+		} else if (operand.equals("<=")) {
+			for (int i = 0; i < range.length; i++) {
+				if (range[i] <= criteria && range[i] >= sumrangelow
+						&& range[i] <= sumrangehigh) {
+					res += range[i];
+				}
+			}
+		} else if (operand.equals(">=")) {
+			for (int i = 0; i < range.length; i++) {
+				if (range[i] >= criteria && range[i] >= sumrangelow
+						&& range[i] <= sumrangehigh) {
+					res += range[i];
+				}
+			}
+		}
+		return res;
 	}
 }
