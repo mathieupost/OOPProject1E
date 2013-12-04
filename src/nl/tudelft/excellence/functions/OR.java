0590b8f004bdff4e80a2dbb23669f72b6d6def22
wdenhertog <wmdenhertog@gmail.com>
2013-12-04 14:46:34 +0100

    Added OR function class

---
+++ src/nl/tudelft/excellence/functions/OR.java
@@ -0,0 +1,20 @@
+package nl.tudelft.excellence.functions;
+
+/**
+ * Returns true, when 1 or more input values is true, and returns false when all input is false
+ * <b>Syntax:</b> OR(boolean a[, boolean b, boolean c...])
+ */
+
+public class OR extends BooleanFunction{
+	
+	public OR(boolean a, boolean...bs){
+		
+	}
+
+	@Override
+	public boolean execute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
