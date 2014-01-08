2856dacf5852bb776fcde3071834c516b12e264e
Erackron <jorairijsdijk@gmail.com>
2014-01-08 16:53:35 +0100

    Add Utility.GetOS() for platform specific mods

--- src/main/nl/tudelft/excellence/utilities/Utility.java
+++ src/main/nl/tudelft/excellence/utilities/Utility.java
@@ -153,4 +153,23 @@ public class Utility {
 	public static boolean isBoolean(String in){
 		return in.equalsIgnoreCase("true")||in.equalsIgnoreCase("false");
 	}
+
+
+	public static String getOS() {
+		String os = System.getProperty("os.name").toLowerCase();
+
+		if(os.indexOf("mac") >= 0){
+			return "MAC";
+		}
+		else if(os.indexOf("win") >= 0){
+			return "WIN";
+		}
+		else if(os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0){
+			return "LINUX/UNIX";
+		}
+		else if(os.indexOf("sunos") >= 0){
+			return "SOLARIS";
+		}
+		return "";
+	}
 }
