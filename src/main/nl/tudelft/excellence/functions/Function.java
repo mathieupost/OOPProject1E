8635aaaf3ca7a90a4640b240fcf1529ebdfd8aa7
Mathieu Post <mathieupost@gmail.com>
2014-01-15 16:24:58 +0100

    Edit the IllegalFunctionArgumentsException message in Function

--- src/main/nl/tudelft/excellence/functions/Function.java
+++ src/main/nl/tudelft/excellence/functions/Function.java
@@ -3,9 +3,9 @@ package nl.tudelft.excellence.functions;
 import nl.tudelft.excellence.exceptions.IllegalFunctionArgumentsException;
 
 public abstract class Function {
-	public Function(int MIN_ARGS, String... values) throws IllegalFunctionArgumentsException{
-		if(values.length<MIN_ARGS){
-			throw new IllegalFunctionArgumentsException("Insufficient arguments supplied. Need at least "+MIN_ARGS+".");
+	public Function(int MIN_ARGS, String... values) throws IllegalFunctionArgumentsException {
+		if (values.length < MIN_ARGS) {
+			throw new IllegalFunctionArgumentsException("Need at least " + MIN_ARGS + "arguments");
 		}
 	}
 }
