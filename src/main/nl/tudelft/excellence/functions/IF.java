ea5932e7d64476365e72e90f9b989fc69a1d4c09
Mathieu Post <mathieupost@gmail.com>
2014-01-15 17:43:23 +0100

    IF functie dichtgetimmerd

--- src/main/nl/tudelft/excellence/functions/IF.java
+++ src/main/nl/tudelft/excellence/functions/IF.java
@@ -1,6 +1,10 @@
 package nl.tudelft.excellence.functions;
 
 import nl.tudelft.excellence.exceptions.IllegalFunctionArgumentsException;
+import nl.tudelft.excellence.spreadsheet.SpreadSheet;
+import nl.tudelft.excellence.spreadsheet.cells.Cell;
+import nl.tudelft.excellence.spreadsheet.cells.CellCoord;
+import nl.tudelft.excellence.utilities.Utility;
 
 /**
  * Converts a String into a logical condition and tests that condition
@@ -16,16 +20,28 @@ public class IF extends StringFunction {
 	private String iffalse;
 	private boolean isLogicValue;
 	
-	final static int MIN_ARGS = 3;
+	final static int MIN_ARGS = 1;
 
 	public IF(String...strings) throws IllegalFunctionArgumentsException{
-		super(MIN_ARGS,strings);
-		logictest = strings[0].trim();
+		super(MIN_ARGS, strings);
+
+		if (strings.length == 2) {
+			throw new IllegalFunctionArgumentsException("This function needs 1 argument or 3 or more arguments");
+		}
+
+		logictest = Utility.getValue(strings[0].trim());
 		isLogicValue = logictest.equalsIgnoreCase("true") || logictest.equalsIgnoreCase("false");
-			
-		this.logictest = logictest.replaceFirst("((!?=)|((<|>)=?))", "|$0|");;
-		iftrue = strings[1];
-		iffalse = strings[2];
+
+		this.logictest = logictest.replaceFirst("((!?=)|((<|>)=?))", "|$0|");
+		System.out.println("logictest = " + logictest);
+
+		if (strings.length >= 3) {
+			iftrue = strings[1];
+			iffalse = strings[2];
+		} else {
+			iftrue = "true";
+			iffalse = "false";
+		}
 	}
 
 	@Override
@@ -34,9 +50,9 @@ public class IF extends StringFunction {
 			return (Boolean.parseBoolean(logictest))?iftrue:iffalse;
 		}
 		
-		String[] split = logictest.split("\\|");
-		a = Double.parseDouble(split[0]);
-		b = Double.parseDouble(split[2]);
+		String[] split = logictest.split("\\|");	
+		a = Double.parseDouble(Utility.getValue(split[0]));
+		b = Double.parseDouble(Utility.getValue(split[2]));
 		
 		if (split[1].equals("=")) {
 			if (a == b) {
