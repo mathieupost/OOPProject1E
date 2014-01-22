2f73c88c09a6d340aab752c9491f49834db3ad16
Casper van der Hout <C.T.vanderhout@student.tudelft.nl>
2014-01-22 16:01:11 +0100

    finished tests for CountIf and SumIf
    All tests for functions are now done

--- src/test/nl/tudelft/excellence/functions/CountIfTest.java
+++ src/test/nl/tudelft/excellence/functions/CountIfTest.java
@@ -8,8 +8,18 @@ public class CountIfTest {
 
 	@Test
 	public void test() {
-		COUNTIF c = new COUNTIF("50", "<", "50", "20", "80", "21.5", "20");
+		COUNTIF c = new COUNTIF("50", "=", "50", "20", "80", "21.5", "20");
+		assertEquals(c.execute(), 1, 0);
+		c = new COUNTIF("50", "!=", "50", "20", "80", "21.5", "20");
+		assertEquals(c.execute(), 4, 0);
+		c = new COUNTIF("50", "<", "50", "20", "80", "21.5", "20");
 		assertEquals(c.execute(), 3, 0);
+		c = new COUNTIF("50", ">", "50", "20", "80", "21.5", "20");
+		assertEquals(c.execute(), 1, 0);
+		c = new COUNTIF("50", "<=", "50", "20", "80", "21.5", "20");
+		assertEquals(c.execute(), 4, 0);
+		c = new COUNTIF("50", ">=", "50", "20", "80", "21.5", "20");
+		assertEquals(c.execute(), 2, 0);
 	}
 
 }
--- src/test/nl/tudelft/excellence/functions/SumIfTest.java
+++ src/test/nl/tudelft/excellence/functions/SumIfTest.java
@@ -9,14 +9,17 @@ public class SumIfTest {
 	@Test
 	public void test() {
 		//test without sumrange
-		SUMIF test = new SUMIF("6","<","1","2","3","4","5","6","7");
-		SUMIF test2 = new SUMIF("6","<=","1","2","3","4","5","6","7");
-		SUMIF test3 = new SUMIF("6","!=","1","2","3","4","5","6","7");
-		assertEquals(test.execute(),(1+2+3+4+5),0);
-		assertEquals(test2.execute(),(1+2+3+4+5+6),0);
-		assertEquals(test3.execute(),(1+2+3+4+5+7),0);
-		
-		
+		SUMIF test = new SUMIF("6","=","1","2","3","4","5","6","7");
+		SUMIF test2 = new SUMIF("6","!=","1","2","3","4","5","6","7");
+		SUMIF test3 = new SUMIF("6","<","1","2","3","4","5","6","7");
+		SUMIF test4 = new SUMIF("6",">","1","2","3","4","5","6","7");
+		SUMIF test5 = new SUMIF("6","<=","1","2","3","4","5","6","7");
+		SUMIF test6 = new SUMIF("6",">=","1","2","3","4","5","6","7");
+		assertEquals(test.execute(),6,0);
+		assertEquals(test2.execute(),(1+2+3+4+5+7),0);
+		assertEquals(test3.execute(),(1+2+3+4+5),0);
+		assertEquals(test4.execute(),7,0);
+		assertEquals(test5.execute(),(1+2+3+4+5+6),0);
+		assertEquals(test6.execute(),(6+7),0);		
 	}
-
 }
